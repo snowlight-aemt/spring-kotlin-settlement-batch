9b92694a720f663f9dc0be51b6968ba1959456d4
snowlight-aemt <it.s.aemt@gmail.com>
2024-01-05 22:56:31 +0900

    INIT

---
+++ src/main/kotlin/me/snowlight/springsettlementbatch/SpringSettlementBatchApplication.kt
@@ -0,0 +1,11 @@
+package me.snowlight.springsettlementbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSettlementBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSettlementBatchApplication>(*args)
+}
---
+++ src/test/kotlin/me/snowlight/springsettlementbatch/SpringSettlementBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.snowlight.springsettlementbatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSettlementBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
