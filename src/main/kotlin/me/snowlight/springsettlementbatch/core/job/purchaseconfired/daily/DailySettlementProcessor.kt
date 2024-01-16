f3d85a6f6f1d67261d517fb972a5f49d265562d1
snowlight-aemt <it.s.aemt@gmail.com>
2024-01-16 23:00:06 +0900

    정산 금액 계산기 클래스 리팩토링

--- src/main/kotlin/me/snowlight/springsettlementbatch/core/job/purchaseconfired/daily/DailySettlementProcessor.kt
+++ src/main/kotlin/me/snowlight/springsettlementbatch/core/job/purchaseconfired/daily/DailySettlementProcessor.kt
@@ -2,6 +2,7 @@ package me.snowlight.springsettlementbatch.core.job.purchaseconfired.daily
 
 import me.snowlight.springsettlementbatch.domain.collection.CommissionAmountCalculator
 import me.snowlight.springsettlementbatch.domain.collection.PgSalesAmountCalculator
+import me.snowlight.springsettlementbatch.domain.collection.PositiveDailySettlementCollection
 import me.snowlight.springsettlementbatch.domain.collection.TaxCalculator
 import me.snowlight.springsettlementbatch.domain.entity.order.OrderItem
 import me.snowlight.springsettlementbatch.domain.entity.settlement.SettlementDaily
@@ -11,34 +12,6 @@ import java.time.LocalDate
 
 class DailySettlementProcessor: ItemProcessor<OrderItem, SettlementDaily> {
     override fun process(orderItem: OrderItem): SettlementDaily {
-        val orderItemSnapshot = orderItem.orderItemSnapshot
-        val seller = orderItemSnapshot.seller
-        val count = orderItem.orderCount
-        val countToBigDecimal = count.toBigDecimal()
-
-        // 세금 계산
-        val taxCalculator = TaxCalculator(orderItemSnapshot)
-        val taxAmount: BigDecimal = taxCalculator.getTaxAmount().multiply(countToBigDecimal)
-
-        // 정산금액에 필요한 데이터 만들기
-        val pgSalesAmountCalculator = PgSalesAmountCalculator(orderItemSnapshot)
-        val pgSalesAmount: BigDecimal = pgSalesAmountCalculator.getPgSaleAmount().multiply(countToBigDecimal)
-        val commissionAmountCalculator = CommissionAmountCalculator(orderItemSnapshot)
-        val commissionAmount: BigDecimal = commissionAmountCalculator.getCommissionAmount().multiply(countToBigDecimal)
-
-        return SettlementDaily(
-            settlementDate = LocalDate.now(),
-            orderNo = orderItem.orderNo,
-            orderCount = count,
-            orderItemNo = orderItem.id,
-            sellerNo = orderItemSnapshot.sellerNo,
-            sellerBusinessNo = seller.businessNo,
-            sellerName = seller.sellerName,
-            sellType = seller.sellType,
-            taxType = orderItemSnapshot.taxType,
-            taxAmount = taxAmount,
-            commissionAmount = commissionAmount,
-            pgSalesAmount = pgSalesAmount,
-        )
+        return PositiveDailySettlementCollection(orderItem).getSettlementDaily()
     }
 }
